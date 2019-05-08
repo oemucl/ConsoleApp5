ce3a0db006244f7ac5999bf9d9de99dc655517d0
Ole Egebjerg Mikkelsen <oem@ucl.dk>
2019-05-08 21:19:45 +0200

    Second commit

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello one world, one family, one philosophy");
             Console.ReadKey();
         }
     }
